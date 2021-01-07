a9079c9b23fd125d204973f5fd0649c976f21572
vale <60008256+valeeeeyyyyy@users.noreply.github.com>
2021-01-06 21:02:41 -0500

    Create Essentials.php

---
+++ src/EssentialsX/Essentials.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace EssentialsX;
+
+
+
+use pocketmine\block\EndStone;
+use pocketmine\plugin\PluginBase;
+use pocketmine\utils\Config;
+
+class EssentialsX extends PluginBase{
+
+    /**
+     * @var EssentialsX $instance
+     */
+
+    private static $instance;
+    public $data;
+
+    public function onEnable(): void{
+
+        self::$instance = $this;
+        @mkdir($this->getDataFolder());
+        $this->data = new Config($this->getDataFolder(). "config.yml". Config::YAML. [
+            "test" => "1",
+        ]) ;
+
+
+    }
+
+    public static function getInstance(): EssentialsX{
+
+        return self::$instance;
+    }
+}
